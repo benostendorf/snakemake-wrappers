9463e23971e31848e35c3149f6f320d580d2d66b
Benjamin Ostendorf <benjamin.ostendorf@gmail.com>
2020-09-08 16:32:07 -0400

    Update wrapper.py

--- bio/multiqc/wrapper.py
+++ bio/multiqc/wrapper.py
@@ -16,6 +16,8 @@ output_dir = path.dirname(snakemake.output[0])
 output_name = path.basename(snakemake.output[0])
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
+shell("export LC_ALL=C.UTF-8")
+shell("export LANG=C.UTF-8")
 shell(
     "multiqc"
     " {snakemake.params}"
